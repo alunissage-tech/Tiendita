8d38fa9c8eadb5ed70cf11f24bf611e3b294c9a6
BasiliscX <juniornavarro1988@gmail.com>
2024-08-21 22:48:07 -0300

    feat: added swirch theme

--- View/Login.cs
+++ View/Login.cs
@@ -2,24 +2,20 @@
 using MaterialSkin.Controls;
 using System;
 using System.Windows.Forms;
+using View.UserControls;
 
 namespace View
 {
-    public partial class Login : MaterialForm
+    public partial class Login : BaseMaterialForm
     {
         public Login()
         {
             InitializeComponent();
 
-            // Configuración de MaterialSkin
-            var materialSkinManager = MaterialSkinManager.Instance;
-            materialSkinManager.AddFormToManage(this);
-            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new ColorScheme(
-                Primary.Blue600, Primary.Blue700,
-                Primary.Blue200, Accent.LightBlue200,
-                TextShade.WHITE
-            );
+            // Agregar el Header para el cambio de tema
+            Header header = new Header(this);
+            this.Controls.Add(header);
+            header.Dock = DockStyle.Top;
 
             // Asegurar que los controles estén centrados al iniciar
             CenterControls();
@@ -27,6 +23,7 @@ namespace View
             // Establecer la ventana en el centro de la pantalla al iniciar
             this.StartPosition = FormStartPosition.CenterScreen;
         }
+
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
@@ -55,17 +52,14 @@ namespace View
 
         private void CenterControls()
         {
-            // Calcular la posición centrada para cada control, tanto horizontal como verticalmente
             int formCenterX = this.ClientSize.Width / 2;
             int formCenterY = this.ClientSize.Height / 2;
 
-            // Posiciones calculadas para centrar los controles verticalmente
-            int txtUsernameY = formCenterY - (txtUsername.Height + txtPassword.Height + chkRememberMe.Height + btnLogin.Height + 40) / 2; // 40 es la suma de los márgenes entre los controles
-            int txtPasswordY = txtUsernameY + txtUsername.Height + 10; // 10 es el margen entre txtUsername y txtPassword
+            int txtUsernameY = formCenterY - (txtUsername.Height + txtPassword.Height + chkRememberMe.Height + btnLogin.Height + 40) / 2;
+            int txtPasswordY = txtUsernameY + txtUsername.Height + 10;
             int chkRememberMeY = txtPasswordY + txtPassword.Height + 10;
             int btnLoginY = chkRememberMeY + chkRememberMe.Height + 20;
 
-            // Centrando los controles horizontalmente
             txtUsername.Left = formCenterX - txtUsername.Width / 2;
             txtUsername.Top = txtUsernameY;
 
@@ -78,8 +72,7 @@ namespace View
             btnLogin.Left = formCenterX - btnLogin.Width / 2;
             btnLogin.Top = btnLoginY;
 
-            // Alinear las etiquetas con los campos de texto
-            lblUsername.Left = txtUsername.Left - lblUsername.Width - 10; // 10 es un margen
+            lblUsername.Left = txtUsername.Left - lblUsername.Width - 10;
             lblUsername.Top = txtUsername.Top + (txtUsername.Height - lblUsername.Height) / 2;
 
             lblPassword.Left = txtPassword.Left - lblPassword.Width - 10;
