55054f6b5f93f388e2cb911c819ab21d8dd8e16c
BasiliscX <juniornavarro1988@gmail.com>
2024-08-22 18:52:58 -0300

    style: update close buttom in mainform

--- View/MainForm.cs
+++ View/MainForm.cs
@@ -19,10 +19,15 @@ namespace View
             this.StartPosition = FormStartPosition.CenterScreen;
             this.MinimumSize = new Size(800, 600);
 
+            // Enable custom drawing for tabs to add close buttons
+            tabControl.DrawMode = TabDrawMode.OwnerDrawFixed;
+            tabControl.DrawItem += new DrawItemEventHandler(tabControl_DrawItem);
+            tabControl.MouseDown += new MouseEventHandler(tabControl_MouseDown);
+
             // Suscribe to events from Header
-            header.ProductsClicked += (s, e) => OpenTab("Productos", new ProductsView());
-            header.SalesClicked += (s, e) => OpenTab("Ventas", new SalesView());
-            header.InvoicesClicked += (s, e) => OpenTab("facturas", new InvoicesView());
+            header.ProductsClicked += (s, e) => OpenTab("Productos      ", new ProductsView());
+            header.SalesClicked += (s, e) => OpenTab("Ventas       ", new SalesView());
+            header.InvoicesClicked += (s, e) => OpenTab("Facturas      ", new InvoicesView());
 
             header.SetButtonVisibility(isLoginScreen: false);
         }
@@ -45,15 +50,59 @@ namespace View
             }
 
             // Create a new tab page if it doesn't exist
-            TabPage newTab = new TabPage(tabName)
-            {
-                Dock = DockStyle.Fill
-            };
+            TabPage newTab = new TabPage(tabName);
 
             view.Dock = DockStyle.Fill;
             newTab.Controls.Add(view);
             tabControl.TabPages.Add(newTab);
             tabControl.SelectedTab = newTab;
+        }
+
+        /// <summary>
+        /// Custom drawing of tabs to include a close button.
+        /// </summary>
+        private void tabControl_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            TabPage tabPage = tabControl.TabPages[e.Index];
+            Rectangle tabRect = tabControl.GetTabRect(e.Index);
+
+            // Define the padding for the close button
+            int closeButtonSize = 15;
+            int textPadding = closeButtonSize + 10; // Adding extra space for the close button
+
+            // Draw the tab text with some padding
+            TextRenderer.DrawText(e.Graphics, tabPage.Text, tabPage.Font,
+                new Rectangle(tabRect.X, tabRect.Y, tabRect.Width - textPadding, tabRect.Height),
+                Color.Black, TextFormatFlags.Left | TextFormatFlags.VerticalCenter);
+
+            // Calculate the rectangle for the close button
+            Rectangle closeButtonRect = new Rectangle(
+                tabRect.Right - closeButtonSize - 5,
+                tabRect.Top + (tabRect.Height - closeButtonSize) / 2,
+                closeButtonSize,
+                closeButtonSize);
+
+            // Draw the close button ("x")
+            TextRenderer.DrawText(e.Graphics, "x", tabPage.Font, closeButtonRect, Color.Red);
+
+            // Store the rectangle in the Tag property of the TabPage
+            tabPage.Tag = closeButtonRect;
+        }
+
+        /// <summary>
+        /// Handles the MouseDown event to close the tab when the close button is clicked.
+        /// </summary>
+        private void tabControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            for (int i = 0; i < tabControl.TabPages.Count; i++)
+            {
+                Rectangle closeButtonRect = (Rectangle)tabControl.TabPages[i].Tag;
+                if (closeButtonRect.Contains(e.Location))
+                {
+                    tabControl.TabPages.RemoveAt(i);
+                    break;
+                }
+            }
         }
     }
 }
