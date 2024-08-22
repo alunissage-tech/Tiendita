12dc9b16c857474688a2f24d4a56d97e272c3075
BasiliscX <juniornavarro1988@gmail.com>
2024-08-22 19:37:44 -0300

    feat: added background image in mainform

--- View/MainForm.cs
+++ View/MainForm.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using System.Windows.Forms;
 using View.UserControls;
 
@@ -11,6 +12,15 @@ namespace View
     /// </summary>
     public partial class MainForm : BaseMaterialForm
     {
+        /// <summary>
+        /// Panel that is displayed when no tabs are open.
+        /// </summary>
+        private Panel defaultPanel;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainForm"/> class.
+        /// Configures the main UI components and initializes event handlers.
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
@@ -19,12 +29,15 @@ namespace View
             this.StartPosition = FormStartPosition.CenterScreen;
             this.MinimumSize = new Size(800, 600);
 
+            // Initialize the default panel
+            InitializeDefaultPanel();
+
             // Enable custom drawing for tabs to add close buttons
             tabControl.DrawMode = TabDrawMode.OwnerDrawFixed;
-            tabControl.DrawItem += new DrawItemEventHandler(tabControl_DrawItem);
-            tabControl.MouseDown += new MouseEventHandler(tabControl_MouseDown);
+            tabControl.DrawItem += tabControl_DrawItem;
+            tabControl.MouseDown += tabControl_MouseDown;
 
-            // Suscribe to events from Header
+            // Subscribe to events from Header
             header.ProductsClicked += (s, e) => OpenTab("Productos      ", new ProductsView());
             header.SalesClicked += (s, e) => OpenTab("Ventas       ", new SalesView());
             header.InvoicesClicked += (s, e) => OpenTab("Facturas      ", new InvoicesView());
@@ -33,13 +46,45 @@ namespace View
         }
 
         /// <summary>
+        /// Initializes the default panel that is displayed when no tabs are open.
+        /// Attempts to load a background image from a predefined path.
+        /// </summary>
+        private void InitializeDefaultPanel()
+        {
+            defaultPanel = new Panel
+            {
+                Dock = DockStyle.Fill,
+                Visible = true
+            };
+
+            // Get the path of the background image
+            //string imagePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images", "mainform_background.png");
+
+            //if (File.Exists(imagePath))
+            //{
+            //    defaultPanel.BackgroundImage = Image.FromFile(imagePath);
+            //    defaultPanel.BackgroundImageLayout = ImageLayout.Stretch; // Supports resizing
+            //}
+            //else
+            //{
+            //    MessageBox.Show($"Background image not found at:\n{imagePath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+
+            this.Controls.Add(defaultPanel);
+            defaultPanel.BringToFront();
+        }
+
+        /// <summary>
         /// Opens a new tab or selects an existing one.
+        /// If the tab is already open, it brings the existing tab to the front.
         /// </summary>
         /// <param name="tabName">The name of the tab to open.</param>
-        /// <param name="view">The UserControl to display in the tab.</param>
+        /// <param name="view">The <see cref="UserControl"/> to display in the tab.</param>
         private void OpenTab(string tabName, UserControl view)
         {
-            // Check if the tab already exists
+            // Hide the default panel when a tab is opened
+            defaultPanel.Visible = false;
+
             foreach (TabPage tab in tabControl.TabPages)
             {
                 if (tab.Text == tabName)
@@ -60,7 +105,10 @@ namespace View
 
         /// <summary>
         /// Custom drawing of tabs to include a close button.
+        /// Adds a red "x" at the end of the tab title.
         /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Event data that provides information about the drawing.</param>
         private void tabControl_DrawItem(object sender, DrawItemEventArgs e)
         {
             TabPage tabPage = tabControl.TabPages[e.Index];
@@ -90,8 +138,11 @@ namespace View
         }
 
         /// <summary>
-        /// Handles the MouseDown event to close the tab when the close button is clicked.
+        /// Handles the <see cref="MouseDown"/> event to close the tab when the close button is clicked.
+        /// If no tabs remain, the default panel is shown.
         /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">Event data that provides information about the mouse click.</param>
         private void tabControl_MouseDown(object sender, MouseEventArgs e)
         {
             for (int i = 0; i < tabControl.TabPages.Count; i++)
@@ -100,6 +151,12 @@ namespace View
                 if (closeButtonRect.Contains(e.Location))
                 {
                     tabControl.TabPages.RemoveAt(i);
+
+                    // Show the default panel if no tabs are open
+                    if (tabControl.TabPages.Count == 0)
+                    {
+                        defaultPanel.Visible = true;
+                    }
                     break;
                 }
             }
