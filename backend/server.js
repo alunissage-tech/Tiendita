60f96a1bcf1fec62e6a838a985a535f2e3b91c42
BasiliscX <juniornavarro1988@gmail.com>
2024-08-23 12:58:28 -0300

    feat: added vercel prod script

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,19 @@ import express from "express";
 import router from "./src/index.js";
 
 const app = express();
-const PORT = process.env.TIENDITA_API_PORT || 3000;
 
 app.use(express.json());
 app.use("/", router);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.get("/", (_req, res) => {
+  res.send("Welcome to Tiendita API");
 });
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.TIENDITA_API_PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default app;
